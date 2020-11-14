41bd4a76b1447cb02258aee67eb4eee6ea511b0f
Diane <dmnapolitano@gmail.com>
2020-11-14 12:52:58 -0500

    Adding a column to all tables with the event hour in 12-hour time

--- etl.py
+++ etl.py
@@ -3,6 +3,17 @@ import sqlite3
 import pandas
 
 
+def create_hour_12(time_24_str):
+    hour_24 = int(time_24_str.split(":")[0])
+    if hour_24 == 0:
+        return "12 AM"
+    if hour_24 == "12":
+        return "12 PM"
+    if hour_24 < 12:
+        return str(hour_24) + " AM"
+    return str(hour_24 - 12) + " PM"
+
+
 # sheet names are the index
 table_names = ["browse_navigation", "keyword_search", "suggested_search", "content_preview", "content_retrievals"]
 
@@ -17,14 +28,16 @@ user_id TEXT NOT NULL,
 event_time TEXT NOT NULL,
 event_label TEXT NOT NULL,
 second_page BLOB NOT NULL,
-session_duration FLOAT NOT NULL);"""
+session_duration FLOAT NOT NULL,
+event_hour_12 TEXT NOT NULL);"""
 
 keyword_search_create_table = """CREATE TABLE IF NOT EXISTS keyword_search (
 user_id TEXT NOT NULL,
 event_time TEXT NOT NULL,
 query BLOB NOT NULL,
 media_types TEXT NULL,
-session_duration FLOAT NOT NULL);"""
+session_duration FLOAT NOT NULL,
+event_hour_12 TEXT NOT NULL);"""
 
 suggested_search_create_table = """CREATE TABLE IF NOT EXISTS suggested_search (
 user_id TEXT NOT NULL,
@@ -32,7 +45,8 @@ event_time TEXT NOT NULL,
 event_category TEXT NOT NULL,
 event_label BLOB NOT NULL,
 second_page BLOB NOT NULL,
-session_duration FLOAT NOT NULL);"""
+session_duration FLOAT NOT NULL,
+event_hour_12 TEXT NOT NULL);"""
 
 content_preview_create_table = """CREATE TABLE IF NOT EXISTS content_preview (
 user_id TEXT NOT NULL,
@@ -40,7 +54,8 @@ event_time TEXT NOT NULL,
 event_category TEXT NOT NULL,
 event_label BLOB NOT NULL,
 page BLOB NOT NULL,
-session_duration FLOAT NOT NULL);"""
+session_duration FLOAT NOT NULL,
+event_hour_12 TEXT NOT NULL);"""
 
 content_retrievals_create_table = """CREATE TABLE IF NOT EXISTS content_retrievals (
 user_id TEXT NOT NULL,
@@ -48,7 +63,8 @@ event_time TEXT NOT NULL,
 event_category TEXT NOT NULL,
 event_label BLOB NOT NULL,
 retrievals_count INTEGER NOT NULL,
-session_duration FLOAT NOT NULL);"""
+session_duration FLOAT NOT NULL,
+event_hour_12 TEXT NOT NULL);"""
 
 
 with sqlite3.connect(db_file) as connection:
@@ -71,12 +87,17 @@ with sqlite3.connect(db_file) as connection:
     for (i, table) in enumerate(table_names):
         print(table)
         
-        df = pandas.read_excel(original_data_file, sheet_name=i)
+        df = pandas.read_excel(original_data_file, sheet_name=i,
+                               dtype={"Event time" : str, "User_Id" : str})
         if "Session Duration" in df.columns:
             df["Session Duration"] = df["Session Duration"].fillna(0)
+        if "Event time" in df.columns:
+            df["Event time"] = df["Event time"].apply(
+                lambda x : x[0:2] + ":" + x[2:])
+            df["event_hour_12"] = df["Event time"].apply(create_hour_12) 
 
         insert_statement = ("INSERT INTO {} VALUES (".format(table)
-                            + "?, " * (4 if i <= 1 else 5) + "?);")
+                            + "?, " * (5 if i <= 1 else 6) + "?);")
         to_insert = (tuple(row) for (j, row) in df.iterrows())
         cursor = connection.cursor()
         cursor.executemany(insert_statement, to_insert)
